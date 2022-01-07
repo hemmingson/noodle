99c1337c0a54a5a3ff4d0cacf5290f32e53aa413
hemmingson <287969392@qq.com>
2022-01-07 23:58:11 +0900

    feat: add es next

---
+++ esnext/index.js
@@ -0,0 +1,7 @@
+/**
+ * stage 1: https://github.com/tc39/proposals/blob/main/stage-1-proposals.md
+ * finished: https://github.com/tc39/proposals/blob/main/finished-proposals.md
+ */
+
+const arr = [0, 1, 2]
+const getLastArrayItem = (arr) => arr.at(-1)
