ef3efab99d6c4321cfce6a88d0ddc3a37b9ba142
hemmingson <287969392@qq.com>
2022-01-09 22:36:44 +0900

    feat: update es next

--- esnext/index.js
+++ esnext/index.js
@@ -3,5 +3,20 @@
  * finished: https://github.com/tc39/proposals/blob/main/finished-proposals.md
  */
 
-const arr = [0, 1, 2]
+const arr = [0, 1, 2, 3]
+
 const getLastArrayItem = (arr) => arr.at(-1)
+
+arr.groupBy((i) => i > 2 ? 'more than 2' : 'less than 2')
+Array.prototype.groupBy = function(callback) {
+  const object = {}
+  for(let i = 0; i < this.length; i++) {
+    let key = callback(this[i], i, this)
+    if(object[key]) {
+      object[key].push(this[i])
+    }else {
+      object[key] = [this[i]]
+    }
+  }
+  return object
+}
