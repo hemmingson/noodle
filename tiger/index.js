e233faa873f01e2cd3cd0b27d4700473f418008c
hemmingson <287969392@qq.com>
2022-01-27 23:58:21 +0900

    feat: update 2022

--- tiger/index.js
+++ tiger/index.js
@@ -1,1 +1,42 @@
 console.log('🐅 year')
+
+// enum
+const $ = Object.freeze({
+  Remix: 0,
+  Threejs: 1,
+  Rust: 2
+})
+
+class Dev {
+  energy = 100
+
+  eat() {
+    console.log(this.energy)
+    this.energy + 50
+    return this
+  }
+
+  code() {
+    this.energy = 105
+    return this
+  }
+
+  sleep() {
+    this.energy = 0
+    return this
+  }
+}
+let hem = new Dev()
+hem
+  .code()
+  .eat()
+  .code()
+  .sleep()
+  .eat()
+
+hem = {
+  age: 26,
+  lang: 'JavaScript'
+}
+let age, lang
+({ age, lang } = hem)
